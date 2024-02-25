b128067b0b76da24bd99c34980594f9209c41e17
Pritanshu69 <samantaprit1924@gmail.com>
2024-02-25 10:59:21 +0530

    Day1

---
+++ 01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 1928
+var accountEmail = "prit@gmail.com"
+var accountPassword = "yoyo"
+
+accountEmail = "yoto@gmail.com"
+accountPassword = "5454"
+
+// Try to avoid var 
+
+console.table([accountEmail , accountId , accountPassword])
---
+++ 02_datatypes.js
@@ -0,0 +1,13 @@
+"use struct"; // JS NEWER FUCNTION
+
+// alert(hello)  // we are using node js not browser
+
+//datatypes:
+
+// number
+// boolean
+// "string"
+// null => standalone value
+// symbol => unique
+
+console.log(typeof null);
