adf44d10e1e9886330dd8cc9808ce0c83efb93b3
Pritanshu69 <samantaprit1924@gmail.com>
2024-02-26 12:31:44 +0530

    .

--- 03_conversion.js
+++ 03_conversion.js
@@ -7,3 +7,25 @@ let valueInNumber = Number(score);
 console.log(typeof valueInNumber);
 console.log(valueInNumber);
 
+//33 => 33
+//"33abc" => NaN
+//let isLoggedIn = 1
+//let booleanIsLoggedIn = boolean(isloggedIn)
+
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+
+// console.log(typeof stringNumber)
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue)
+
+// let str1 = "hello"
+// let str2 = " pro"
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log(1 + "2") //12
+// console.log(1 + 2 + "2") //32
+// console.log("1" + 1 + 2) //112
