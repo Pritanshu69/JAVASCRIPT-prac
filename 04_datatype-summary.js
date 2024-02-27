817e566cd1e31b14bea57e61c4ba8a24c0e3e057
Pritanshu69 <samantaprit1924@gmail.com>
2024-02-27 10:45:38 +0530

    .

---
+++ 04_datatype-summary.js
@@ -0,0 +1,11 @@
+// primitive datatype
+
+// 7  types: String,Number,Bool,Null,undefined,Symbol,BigInt
+
+//Reference (Non-Primitive)
+
+//Array,Object,Functions  
+
+const myFuntcion = function(){
+    console.log("I am a function");
+}
