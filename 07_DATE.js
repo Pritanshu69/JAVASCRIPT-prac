2d1b50dcfa23258c4c61fde373173e9cb97b3ac9
Pritanshu69 <samantaprit1924@gmail.com>
2024-03-10 10:40:53 +0530

    Commit message: . Add more examples of number and math operations.

---
+++ 07_DATE.js
@@ -0,0 +1,15 @@
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toISOString);
+
+let myCreatedDate = new Date(2023, 0, 23)  //month starts from zero here!
+// console.log(myCreatedDate.toDateString());
+
+let myTimeStamp = Date.now();
+
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
+
+
