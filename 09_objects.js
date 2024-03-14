36351c983bb1c8a620b65dec8cb28d968c2ebd35
Pritanshu69 <samantaprit1924@gmail.com>
2024-03-14 11:14:48 +0530

    .

--- 09_objects.js
+++ 09_objects.js
@@ -1,5 +1,7 @@
 //singleton => object.create()
 
+
+//object literal
 const jsUser = {
     name : "Prit",
     age: 20,
@@ -8,9 +10,14 @@ const jsUser = {
     isLoggedIn: false,
     lastloggedInDay: ["Sunday"]
 }
-console.log(jsUser["email"]) //square notation
+//console.log(jsUser["email"]) //square notation
 
 jsUser.email = "prit@chatgpt.com"
 //object.freeze(jsUser)
 
-console.log(jsUser);
+//console.log(jsUser);
+
+jsUser.greeting = function(){
+    console.log(`Hello js user, ${this.name}`)  //string interpolation
+}
+console.log(jsUser.greeting());
