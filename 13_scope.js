bd3abb283019afc2147a1d0c6915f67d6608d56c
Pritanshu69 <samantaprit1924@gmail.com>
2024-05-03 23:09:07 +0530

    achchha

--- 13_scope.js
+++ 13_scope.js
@@ -1,8 +1,23 @@
 //local scope
-if(true){
+if(true){                   //curly braces means scope
     let a = 10
     const b = 69
 }
 
 //global scope
 const c = 108
+
+//nested scope 
+
+function one(){
+    const username = "Pritanshu"
+
+    function two() {
+        const website ="youtube";
+        console.log(username);
+    }
+    console.log(website);
+
+    two()
+}
+one()
