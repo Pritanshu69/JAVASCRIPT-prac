098979102f48a67daddc16d75e12f25fd7d0326b
Pritanshu69 <samantaprit1924@gmail.com>
2024-05-07 10:45:21 +0530

    .

--- 13_scope.js
+++ 13_scope.js
@@ -1,23 +1,49 @@
 //local scope
-if(true){                   //curly braces means scope
-    let a = 10
-    const b = 69
-}
+// if(true){                   //curly braces means scope
+//     let a = 10
+//     const b = 69
+// }
 
-//global scope
-const c = 108
+// //global scope
+// const c = 108
 
 //nested scope 
 
-function one(){
-    const username = "Pritanshu"
+// function one(){
+//     const username = "Pritanshu"
 
-    function two() {
-        const website ="youtube";
-        console.log(username);
-    }
-    console.log(website);
+//     function two() {
+//         const website ="youtube";
+//         console.log(username);
+//     }
+//     console.log(website);
 
-    two()
+//     two()
+// }
+// one()
+
+if(true){
+    const username = "Pritanshu";
+    if(username === "Pritanshu"){
+        const website = "youtube"
+        console.log(username + website);
+    }    
+        // console.log(website); -> out of the scope
+
 }
-one()
+// console.log(username) -> out of the scope
+
+// ----------INTERESTING------------
+
+function addOne(num){
+    return num + 1
+}
+
+addOne(5)
+
+const addTwo = function(num){    //ANCHOR - another way of writting function sometimes its called expression
+    return num + 2;
+}
+
+addTwo(6)
+
